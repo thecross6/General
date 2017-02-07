526f9c23f1787a589e02cdb1129f5b7868ce55c9
Arturo Laguna <arturo2499872@gmail.com>
2017-02-07 11:58:09 -0600

    Output is correct

--- Software Architecture/HW1/src/PipeAndFilter.java
+++ Software Architecture/HW1/src/PipeAndFilter.java
@@ -1,25 +1,33 @@
-import java.util.Set;
+import java.util.SortedSet;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class PipeAndFilter {
-	private Set<String> words;
-	public void readTextFile(String location) {
+	private SortedSet<String> words = new TreeSet<>();
+	public void readTextFile(String location) throws FileNotFoundException {
 		File file = new File(location);
 		Scanner inputFile = new Scanner(file);
 		while(inputFile.hasNext()) {
-			String word = inputFile.nextLine();
-			System.out.println(word);
+			String word = inputFile.next();
+			word = word.replaceAll("[,!.]", "");
+			words.add(word.toLowerCase());
 		}
-		
 		inputFile.close();
+		Iterator it = words.iterator();
+		while(it.hasNext()) {
+			Object element = it.next();
+			System.out.println(element.toString());	
+		}
 	}
-	public void displaySet(Set<String> words) {
+	public void displaySet(SortedSet<String> words) {
 		return;
 	}
-	public static void main(String []args) {
-	   String location = "/home/arturo/Documents/Github/General/Software Architecture/Hw1/pftest.txt";
-	   PipeAndFilter aclass = new PipeAndFilter();
-	   aclass.readTextFile(location);
+	public static void main(String []args) throws FileNotFoundException {
+	   String location = "/home/arturo/Documents/Github/General/Software Architecture/HW1/pftest.txt";
+	   PipeAndFilter instance = new PipeAndFilter();
+	   instance.readTextFile(location);
    }
 } 
