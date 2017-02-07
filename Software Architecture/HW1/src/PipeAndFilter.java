cbc2730a843d67d9af5369d5e87922de8091989d
Arturo Laguna <arturo2499872@gmail.com>
2017-02-07 10:51:27 -0600

    Updated java class
    - Added text file
    - Checking read function

--- Software Architecture/HW1/src/PipeAndFilter.java
+++ Software Architecture/HW1/src/PipeAndFilter.java
@@ -1,13 +1,25 @@
-import java.util.HashSet;
 import java.util.Set;
+import java.io.File;
+import java.util.Scanner;
 
 public class PipeAndFilter {
-   public static void main(String []args) {
-      System.out.println("Hello World");
-      Set<String> wordSet = new HashSet<String>();
-      wordSet.add("Ass");
-      wordSet.add("Shit");
-      wordSet.add("Ass");
-      System.out.println(wordSet);
+	private Set<String> words;
+	public void readTextFile(String location) {
+		File file = new File(location);
+		Scanner inputFile = new Scanner(file);
+		while(inputFile.hasNext()) {
+			String word = inputFile.nextLine();
+			System.out.println(word);
+		}
+		
+		inputFile.close();
+	}
+	public void displaySet(Set<String> words) {
+		return;
+	}
+	public static void main(String []args) {
+	   String location = "/home/arturo/Documents/Github/General/Software Architecture/Hw1/pftest.txt";
+	   PipeAndFilter aclass = new PipeAndFilter();
+	   aclass.readTextFile(location);
    }
 } 
